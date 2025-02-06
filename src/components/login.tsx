3b0253f9f43e63a7a572adfd42251a44a9790f93
NicolasRaf <nicolasalves136@gmail.com>
2025-02-06 03:58:06 -0300

    feat: debug API url log

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -6,6 +6,8 @@ const Login: React.FC = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
+  console.log('API URL:', process.env.REACT_APP_API_URL);
+
 
   const handleLogin = async (event: React.FormEvent) => {
     event.preventDefault();
@@ -21,28 +23,28 @@ const Login: React.FC = () => {
   };
 
   return (
-      <form onSubmit={handleLogin}>
-    <h2>{process.env.REACT_APP_API_URL || "API URL não definida"}</h2> {/* Caso a variável esteja indefinida */}
-    <label>
-      Usuário:
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        required
-      />
-    </label>
-    <label>
-      Senha:
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
-    </label>
-    <button type="submit">Entrar</button>
-  </form>
+    <form onSubmit={handleLogin}>
+      <h2>Login</h2>
+      <label>
+        Usuário:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Senha:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Entrar</button>
+    </form>
   );
 };
 
