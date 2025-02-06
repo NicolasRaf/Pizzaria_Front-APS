6219c2fb09b1540a8fcfebc85a2ab8646c970123
NicolasRaf <nicolasalves136@gmail.com>
2025-02-06 03:39:34 -0300

    test: dotenv

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 import App from './App.tsx'
 
-//dotenv.config();
+dotenv.config();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
