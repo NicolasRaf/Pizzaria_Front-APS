582d70b12d5acd0b618fbddeccc3fce0767087a8
NicolasRaf <nicolasalves136@gmail.com>
2025-02-06 03:44:13 -0300

    feat: remove dotenv

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import dotenv from 'dotenv';
 import App from './App.tsx'
-
-dotenv.config();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
